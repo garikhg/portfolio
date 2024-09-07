3cc425b000761e877fb6240080b018dfcad58f96
garikhg <garik.28h@gmail.com>
2024-09-07 20:07:29 +0400

    Update tailwindcss and vite config fix vite additionalData change src/styles/variables.scss to global.scss, install tailwindcss-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.php",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./*.php",
+        "./inc/**/*.php",
+        "./template-parts/**/*.php",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px'
+            }
+        },
+        fontFamily: {
+            heading: ['"League Spartan", sans-serif'],
+            body: ['"Inter", sans-serif'],
+        },
+        backgroundColor: {
+            cream: 'var(--ang--cream)',
+            'frost-gray': 'var(--ang--frost-gray)',
+            'light-gray': 'var(--ang--light-gray)',
+            background: 'var(--ang--frost-gray)',
+            black: 'var(--ang--black)',
+        },
+        extend: {
+            colors: {
+                background: 'var(--ang--frost-gray)',
+            }
+        },
+    },
+    plugins: [
+        require('tailwindcss-animate'),
+    ],
 }
 
