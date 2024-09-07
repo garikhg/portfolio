5f978cbecfea524dc3aaa72ae7d833c1972ab224
garikhg <garik.28h@gmail.com>
2024-09-07 20:09:29 +0400

    Update footer added file documentation comment

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<?php?><?php wp_footer(); ?></body></html>
+<?php/** * The template for displaying the footer * * Contains the closing of the #content div and all content after. * * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials * */?><footer id="colophon" class="site-footer mt-auto">    <div class="container">        Footer    </div></footer></div><!--#page--><?php wp_footer(); ?></body></html>
