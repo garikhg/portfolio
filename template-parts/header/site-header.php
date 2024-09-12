ef209f88f270a2285de5e09cf6449342d473d7e7
garikhg <garik.28h@gmail.com>
2024-09-12 22:52:25 +0400

    Update site header animation

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -3,9 +3,9 @@
  * Displays the site header.
  */
 ?>
-<header id="masthead" class="site-banner sticky top-6 py-6 z-50">
+<header id="masthead" class="site-header sticky top-0 py-6 z-50">
     <div class="container">
-        <div class="bg-background flex items-center justify-between rounded-full h-16 px-4 md:px-6">
+        <div class="backdrop-blur-sm bg-background flex items-center justify-between rounded-full h-16 px-4 md:px-6">
             <?php get_template_part('template-parts/header/site-branding') ?>
             <?php get_template_part('template-parts/header/site-navigation') ?>
         </div>
