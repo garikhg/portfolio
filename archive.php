c1003f945b0ac57c09fd9264f8f413388fb391cb
garikhg <garik.28h@gmail.com>
2024-09-15 21:31:46 +0400

    Added global template archive

---
+++ archive.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+
+get_header();
+?>
+
+
+<?php get_footer(); ?>
