e62a3ff24f523cde520fb2aee2f9e213d10fecae
garikhg <garik.28h@gmail.com>
2024-09-19 22:27:41 +0400

    Update text-link and project heading styles

--- template-parts/content/content-portfolio.php
+++ template-parts/content/content-portfolio.php
@@ -5,7 +5,11 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
 
-$wrapper_class = 'portfolio-item-wrap w-1/2 grid-item apps'
+$wrapper_class = 'portfolio-item-wrap w-1/2 grid-item apps';
+
+$cover_text_color        = get_field( 'title_color', get_the_ID() );
+$project_heading_styles  = $cover_text_color ? ' style="--text: ' . $cover_text_color . ';"' : '';
+
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( $wrapper_class ); ?> data-tilt-perspective="60000">
     <div class="portfolio-item card p-5">
@@ -19,41 +23,36 @@ $wrapper_class = 'portfolio-item-wrap w-1/2 grid-item apps'
                 </a>
             </div>
 
-            <!--<div class="overlay-details top fade-down absolute top-0 p-[3vh]">
-                <button class="w-14 h-14 flex items-center justify-center text-primary-foreground" type="button">
-                    <span><i data-lucide="maximize" class="icon icon-maximize"></i></span>
-                    <span class="button-text sr-only"><?php /*esc_html_e( 'Show', 'portfolio' ); */?></span>
-                </button>
-            </div>-->
-
             <div class="overlay-details w-full absolute bottom-0 p-[3vh]">
-                <div class="heading relative z-10">
-                    <h4 class="text-2xl text-primary font-semibold"><?php the_title() ?></h4>
+                <div class="heading relative z-10"<?php echo $project_heading_styles; ?>>
+                    <h4 class="text-2xl font-semibold">
+                        <a href="<?php echo get_permalink() ?>"><?php the_title() ?></a>
+                    </h4>
                     <div class="show-project mt-2">
                         <div class="project-category__holder">
                             <ul class="project-category__list flex gap-1.5">
                                 <li>
-                                    <a href="#"
-                                       class="text-link text-primary text-sm project-category__link inline-block">
+                                    <a href="#" class="text-link text-sm project-category__link inline-block">
                                         <span class="category-text">Identity</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href="#"
-                                       class="text-link text-primary text-sm project-category__link inline-block">
+                                       class="text-link text-sm project-category__link inline-block">
                                         <span class="category-text">Mockup</span>
                                     </a>
                                 </li>
                             </ul>
                         </div>
-                        <div class="show-project__link text-primary font-semibold">
-                            <a href="<?php echo get_permalink() ?>" class="text-link text-primary">
+
+                        <div class="show-project__link font-semibold">
+                            <a href="<?php echo get_permalink() ?>" class="text-link">
                                 <span class="text"><?php esc_html_e( 'Show project', 'portfolio' ); ?></span>
                             </a>
                         </div>
                     </div>
                 </div>
-                <span class="block w-full h-full bg-gradient-to-t from-primary/40 to-transparent absolute z-0 top-0 left-0 opacity-30"></span>
+                <span class="block w-full h-full bg-gradient-to-t from-gray-950/90 to-transparent absolute z-0 top-0 left-0"></span>
             </div>
         </div>
     </div>
