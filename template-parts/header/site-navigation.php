bdac422f5411339afb1accdc6e3bbee27823f1bb
garikhg <garik.28h@gmail.com>
2024-09-22 23:42:43 +0400

    - Update Mobile menu - Change Hero slider speed from 2500 to 1500

--- template-parts/header/site-navigation.php
+++ template-parts/header/site-navigation.php
@@ -3,16 +3,32 @@
  * Displays the site navigation.
  */
 ?>
-<nav id="site-navigation" class="primary-navigation relative flex" aria-label="<?php esc_attr_e( 'Primary menu', 'portfolio' ) ?>">
-	<div class="menu-button-container -mr-2 md:-mr-3">
-		<button id="primary-mobile-menu" class="w-14 h-14 flex justify-center items-center rounded-full bg-light-gray lg:hidden">
-            <span class="block">
-                <span class="icon-hamburger w-6 block h-0.5 bg-black my-1.5"></span>
-                <span class="icon-hamburger w-4 block h-0.5 bg-black my-1.5"></span>
-            </span>
-			<span class="sr-only"><?php esc_html_e( 'Menu', 'portfolio' ); ?></span>
-		</button>
-	</div>
+<nav id="site-navigation" class="primary-navigation relative flex"
+     aria-label="<?php esc_attr_e( 'Primary menu', 'portfolio' ) ?>">
+    <div class="menu-button-container -mr-2 md:-mr-3">
+        <label for="menu-drawer" class="btn btn-circle swap swap-rotate drawer-button lg:hidden">
+            <!--<input type="checkbox"/>-->
+            <input id="menu-drawer" type="checkbox" class="drawer-toggle" />
+            <!-- hamburger icon -->
+            <svg class="swap-off fill-current"
+                 xmlns="http://www.w3.org/2000/svg"
+                 width="32"
+                 height="32"
+                 viewBox="0 0 512 512">
+                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/>
+            </svg>
+
+            <!-- close icon -->
+            <svg class="swap-on fill-current"
+                 xmlns="http://www.w3.org/2000/svg"
+                 width="32"
+                 height="32"
+                 viewBox="0 0 512 512">
+                <polygon
+                        points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/>
+            </svg>
+        </label>
+    </div>
 	<?php wp_nav_menu(
 		array(
 			'theme_location'  => 'primary',
