84ce91ad2ef6f3f5133168d32a37b4829915a734
garikhg <garik.28h@gmail.com>
2024-09-22 23:48:30 +0400

    - Added site-navigation-drawer template

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -3,11 +3,12 @@
  * Displays the site header.
  */
 ?>
+<?php get_template_part( 'template-parts/header/site-navigation-drawer' ) ?>
 <header id="masthead" class="site-header sticky top-0 py-6 z-50">
     <div class="container">
         <div class="backdrop-blur-sm bg-background flex items-center justify-between rounded-full h-16 px-4 md:px-6">
-            <?php get_template_part('template-parts/header/site-branding') ?>
-            <?php get_template_part('template-parts/header/site-navigation') ?>
+			<?php get_template_part( 'template-parts/header/site-branding' ) ?>
+			<?php get_template_part( 'template-parts/header/site-navigation' ) ?>
         </div>
     </div>
 </header>
---
+++ template-parts/header/site-navigation-drawer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Displays the site navigation drawer.
+ */
+
+$nav_menu = wp_nav_menu(
+	array(
+		'theme_location' => 'primary',
+		'menu_class'     => 'menu-wrapper menu text-base-content text-base w-full p-4',
+		'container'      => false,
+		'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'fallback_cb'    => false,
+		'echo'           => false,
+	)
+);
+
+if ( ! $nav_menu ) {
+	return;
+}
+?>
+<div class="drawer z-[100]">
+    <input id="menu-drawer" type="checkbox" class="drawer-toggle"/>
+    <div class="drawer-side">
+        <label for="menu-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <div class="bg-base-200 w-80 min-h-full">
+            <h5 class="text-lg font-semibold text-gray-700 font-heading border-b border-base-300 p-4 px-8">
+				<?php if ( is_front_page() ): ?>
+					<?php echo esc_html__( 'Menu', 'portfolio' ) ?>
+				<?php else: ?>
+					<?php the_title() ?>
+				<?php endif; ?>
+            </h5>
+
+			<?php echo $nav_menu ?>
+        </div>
+    </div>
+</div>
